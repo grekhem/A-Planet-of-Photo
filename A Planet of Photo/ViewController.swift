41fdc28182e3ee4d6ee5dc099f6400982cd862a1
Grekhem <Grekhem9@gmail.com>
2022-01-09 20:10:52 +0700

    Initial Commit

---
+++ A Planet of Photo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A Planet of Photo
+//
+//  Created by Grekhem on 09.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
